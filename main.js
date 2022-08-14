9a94388fd9d7d34217a07a171c91240893ca4974
Arhan Inam <91776437+ArhanInam@users.noreply.github.com>
2022-08-14 01:45:21 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function Buynoise(){
+    window.location.href ="https://www.gonoise.com/products/noise-colorfit-pro-4-full-touch-control-smart-watch";
+}
+
+function Buywatch(){
+    window.location.href="https://store.goqii.com/vital4?utm_source=google&utm_medium=cpc&utm_campaign=Sok_GOQii_PerformanceMax_Wearables&gclid=CjwKCAjwi8iXBhBeEiwAKbUofQusCYqN9sIxSxTs83JgalUknuYUGmZCnHX9qC1AS0EH4DRTIIkfNxoCsqcQAvD_BwE";
+}
